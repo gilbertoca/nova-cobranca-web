d792de911174ccb9f599a97783dbb7ce6b479fd8
Gilberto Caetano de Andrade <gilbertoca@gmail.com>
2016-10-21 16:41:35 -0300

    Ajustes na verificação do teste de geração de boleto; removendo arquivo temporário

--- src/test/java/com/andreitoledo/cobranca/util/boleto/bopepo/BopepoEmissorBoletoTest.java
+++ src/test/java/com/andreitoledo/cobranca/util/boleto/bopepo/BopepoEmissorBoletoTest.java
@@ -14,42 +14,44 @@ import com.andreitoledo.cobranca.model.ContaBancaria;
 import com.andreitoledo.cobranca.model.Sacado;
 import com.andreitoledo.cobranca.util.boleto.EmissorBoleto;
 import com.andreitoledo.cobranca.util.modulo11.GeradorDigitoVerificador;
+import static org.junit.Assert.assertTrue;
 
 public class BopepoEmissorBoletoTest {
 
-	private EmissorBoleto emissorBoleto;
-	
-	@Before
-	public void init() {
-		GeradorDigitoVerificador geradorDigitoVerificador = new GeradorDigitoVerificador();
-		emissorBoleto = new BopepoEmissorBoleto(geradorDigitoVerificador);
-	}
-	
-	@Test
-	public void deve_gerar_boleto_em_arquivo() throws Exception {
-		Cedente cedenteSistema = new Cedente();
-		cedenteSistema.setNome("AT Systems");
-		cedenteSistema.setCnpj("10.687.566/0001-97");
-		ContaBancaria contaBancaria = new ContaBancaria();
-		contaBancaria.setAgencia(1111);
-		contaBancaria.setDigitoAgencia("0");
-		contaBancaria.setNumero(2222);
-		contaBancaria.setDigitoConta("9");
-		contaBancaria.setCodigoCarteira(6);
-		cedenteSistema.setContaBancaria(contaBancaria);
-		
-		Cobranca cobrancaSistema = new Cobranca();
-		cobrancaSistema.setCodigo(1L);
-		cobrancaSistema.setDataVencimento(new Date());
-		cobrancaSistema.setValor(new BigDecimal("200.22"));
-		
-		Sacado sacado = new Sacado();
-		sacado.setNome("Maria Santos");
-		cobrancaSistema.setSacado(sacado);
-		
-		File boleto = this.emissorBoleto.gerarBoletoEmArquivo("boletoTeste1.pdf", cedenteSistema, cobrancaSistema);
-		Desktop desktop = Desktop.getDesktop();
-		desktop.open(boleto);
-	}
-	
+    private EmissorBoleto emissorBoleto;
+
+    @Before
+    public void init() {
+        GeradorDigitoVerificador geradorDigitoVerificador = new GeradorDigitoVerificador();
+        emissorBoleto = new BopepoEmissorBoleto(geradorDigitoVerificador);
+    }
+
+    @Test
+    public void deve_gerar_boleto_em_arquivo() throws Exception {
+        Cedente cedenteSistema = new Cedente();
+        cedenteSistema.setNome("AT Systems");
+        cedenteSistema.setCnpj("10.687.566/0001-97");
+        ContaBancaria contaBancaria = new ContaBancaria();
+        contaBancaria.setAgencia(1111);
+        contaBancaria.setDigitoAgencia("0");
+        contaBancaria.setNumero(2222);
+        contaBancaria.setDigitoConta("9");
+        contaBancaria.setCodigoCarteira(6);
+        cedenteSistema.setContaBancaria(contaBancaria);
+
+        Cobranca cobrancaSistema = new Cobranca();
+        cobrancaSistema.setCodigo(1L);
+        cobrancaSistema.setDataVencimento(new Date());
+        cobrancaSistema.setValor(new BigDecimal("200.22"));
+
+        Sacado sacado = new Sacado();
+        sacado.setNome("Maria Santos");
+        cobrancaSistema.setSacado(sacado);
+
+        File boleto = this.emissorBoleto.gerarBoletoEmArquivo("boletoTeste1.pdf", cedenteSistema, cobrancaSistema);
+        
+        assertTrue(boleto.exists());
+        boleto.deleteOnExit();
+    }
+
 }
